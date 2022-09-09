6aff0864c8bd0a6d580bbe9bae8e8797b360b056
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-10 02:58:38 +0600

    data fetch from frontedn

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
 const ChatPage = () => {
+    const [chats, setChats] = useState([])
 
     const fetchChats = async () => {
-        const data = await axios.get('http://localhost:5000/api/chat')
-        console.log(data)
+        const {data} = await axios.get('http://localhost:5000/api/chat')
+        setChats(data)
     }
 
     useEffect(() => {
@@ -14,7 +15,9 @@ const ChatPage = () => {
 
     return (
         <div>
-            This is ChatPage    
+            {
+                chats.map(c => <div key={c.chatName}>{c.chatName}</div>)
+            }
         </div>
     );
 };
