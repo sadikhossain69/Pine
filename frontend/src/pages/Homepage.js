fe719e65ecf768d2fd3655f4b0cb454e57cb4cd2
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-10 18:35:26 +0600

    login and signup page crated

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'    
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const Homepage = () => {
     return (
@@ -24,10 +26,10 @@ const Homepage = () => {
           </TabList>
           <TabPanels>
             <TabPanel>
-              {/* <Login /> */}
+              <Login />
             </TabPanel>
             <TabPanel>
-              {/* <Signup /> */}
+              <Signup />
             </TabPanel>
           </TabPanels>
         </Tabs>
