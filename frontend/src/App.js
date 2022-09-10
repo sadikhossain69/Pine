39c4b725f6e109abd893b3a64de9f670d2e4a9c4
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-10 18:31:27 +0600

    login and signup page some design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import { Button } from '@chakra-ui/button'
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
-    <div>
-      <Button colorScheme={"blue"} >Button</Button>
+    <div className='App'>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/chats' element={<ChatPage />} />
