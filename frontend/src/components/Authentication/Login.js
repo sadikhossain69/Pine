e0c13199ce99eb48b51a0763b2a630d59a3feedf
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-10 23:42:48 +0600

    login functionality done

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,13 +1,66 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, useToast, VStack } from '@chakra-ui/react';
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [show, setShow] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const toast = useToast()
 
-    const submitHandler = () => {
+    const submitHandler = async() => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
 
+        // console.log(email, password);
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                "http://localhost:5000/api/user/login",
+                { email, password },
+                config
+            );
+
+            // console.log(JSON.stringify(data));
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            navigate("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
     }
 
     return (
@@ -16,14 +69,14 @@ const Login = () => {
                 <FormLabel>
                     Email
                 </FormLabel>
-                <Input placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
+                <Input placeholder='Enter Your Email' defaultValue={email} onChange={(e) => setEmail(e.target.value)} />
             </FormControl>
             <FormControl id='password' isRequired>
                 <FormLabel>
                     Password
                 </FormLabel>
                 <InputGroup>
-                    <Input placeholder='Enter Your Email' type={show ? 'text' : "password"} onChange={(e) => setPassword(e.target.value)} />
+                    <Input placeholder='Enter Your Password' defaultValue={password} type={show ? 'text' : "password"} onChange={(e) => setPassword(e.target.value)} />
                     <InputRightElement width={'4.5rem'}>
                         <Button onClick={() => setShow(!show)} h={'1.75rem'} size="sm">
                             {
@@ -34,22 +87,23 @@ const Login = () => {
                 </InputGroup>
             </FormControl>
             <Button
-            colorScheme={"blue"}
-            width="100%"
-            style={{marginTop: 15}}
-            onClick={submitHandler}
+                colorScheme={"blue"}
+                width="100%"
+                style={{ marginTop: 15 }}
+                onClick={submitHandler}
+                isLoading={loading}
             >
                 Login
             </Button>
             <Button
-            variant={'solid'}
-            colorScheme={"red"}
-            width="100%"
-            style={{marginTop: 15}}
-            onClick={() => {
-                setEmail("guest@example.com")
-                setPassword("123456")
-            }}
+                variant={'solid'}
+                colorScheme={"red"}
+                width="100%"
+                style={{ marginTop: 15 }}
+                onClick={() => {
+                    setEmail("guest@example.com")
+                    setPassword("123456")
+                }}
             >
                 Login With Guest User
             </Button>
