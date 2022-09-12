d9b2e0b7acb1492fd2898a38c6d822dbfd6067a2
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-13 01:54:25 +0600

    fetchChats created

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { accessChat } = require("../controllers/chatControllers")
+const { accessChat, fetchChats } = require("../controllers/chatControllers")
 const { protect } = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
 router.route('/').post(protect, accessChat)
-// router.route('/').get(protect, fetchChat)
+router.route('/').get(protect, fetchChats)
 // router.route('/group').post(protect, createGroupChat)
 // router.route('/rename').put(protect, remaneGroup)
 // router.route('/groupremove').put(protect, removeFromGroup)
