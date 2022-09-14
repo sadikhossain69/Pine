ca67606a89bcdca53257bfd914ae6aa4f8a43805
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-15 00:01:04 +0600

    remove from group api created

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -154,10 +154,37 @@ const addToGroup = asyncHandler(async (req, res) => {
     }
 });
 
+// remove someone from the group
+const removeFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+
+    // check if the requester is admin
+
+    const removed = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $pull: { users: userId },
+        },
+        {
+            new: true,
+        }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+
+    if (!removed) {
+        res.status(404);
+        throw new Error("Chat Not Found");
+    } else {
+        res.json(removed);
+    }
+});
+
 module.exports = {
     accessChat,
     fetchChats,
     createGroupChat,
     renameGroup,
     addToGroup,
+    removeFromGroup
 }
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup } = require("../controllers/chatControllers")
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require("../controllers/chatControllers")
 const { protect } = require("../middlewares/authMiddleware")
 
 const router = express.Router()
@@ -9,6 +9,6 @@ router.route('/').get(protect, fetchChats)
 router.route('/group').post(protect, createGroupChat)
 router.route('/rename').put(protect, renameGroup)
 router.route('/groupadd').put(protect, addToGroup)
-// router.route('/groupremove').put(protect, removeFromGroup)
+router.route('/groupremove').put(protect, removeFromGroup)
 
 module.exports = router
